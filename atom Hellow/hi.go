fdf6d436a1db897de9814869d6b89c65240554b8
unknown <skyVortex@Rip.bgb1.lan>
2019-02-06 14:37:56 +0800

    ok

--- atom Hellow/hi.go
+++ atom Hellow/hi.go
@@ -1,16 +1,7 @@
 package main
-<<<<<<< HEAD
 
 import "fmt"
 
 func main() {
 	fmt.Println("HI")
-=======
-#include {
-  "frm"
-}
-func main() {
-	frm.println("HI")
->>>>>>> 2abaa29157fc06c24be2667bb40e756a5e6c8db8
-
 }
